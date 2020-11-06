5f5ae6d824bab27256135f6e29a6a18c0301fec5
daiaji <you@example.com>
2020-11-07 00:30:25 +0800

    虽然简陋但能用，我要去睡觉了

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,21 @@
-FROM --platform=${TARGETPLATFORM} alpine:latest
-LABEL maintainer "V2Fly Community <dev@v2fly.org>"
+FROM alpine AS builder
 
 WORKDIR /root
-ARG TARGETPLATFORM
-COPY v2ray.sh /root/v2ray.sh
+RUN apk update \
+    && apk add --no-cache bash tzdata openssl ca-certificates curl unzip \
+    && mkdir -p /etc/v2ray /usr/local/share/v2ray /var/log/v2ray \
+    && curl -fsSL https://raw.githubusercontent.com/daiaji/v2fly-podman/master/v2ray.sh | bash \
+    && curl -fsSLo /usr/local/share/v2ray/h2y.dat https://raw.githubusercontent.com/ToutyRater/V2Ray-SiteDAT/master/geofiles/h2y.dat
 
-RUN set -ex \
-	&& apk add --no-cache tzdata openssl ca-certificates \
-	&& mkdir -p /etc/v2ray /usr/local/share/v2ray /var/log/v2ray \
-	&& chmod +x /root/v2ray.sh \
-	&& /root/v2ray.sh "${TARGETPLATFORM}"
+FROM scratch
+
+LABEL maintainer "Darian Raymond <admin@v2ray.com>"
+
+COPY --from=builder /usr/bin/v2ray /usr/bin/v2ctl /usr/bin/
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
+COPY --from=builder /etc/v2ray/config.json /etc/v2ray/config.json
+COPY --from=builder /usr/local/share/v2ray/geosite.dat /usr/local/share/v2ray/geoip.dat /usr/local/share/v2ray/h2y.dat /usr/local/share/v2ray/
 
 VOLUME /etc/v2ray
-CMD [ "/usr/bin/v2ray", "-config", "/etc/v2ray/config.json" ]
+ENTRYPOINT [ "v2ray" ]
+CMD [ "-config", "/etc/v2ray/config.json" ]
--- v2ray.sh
+++ v2ray.sh
@@ -1,48 +1,68 @@
 #!/bin/sh
 
-# Set ARG
-PLATFORM=$1
-if [ -z "$PLATFORM" ]; then
-    ARCH="64"
-else
-    case "$PLATFORM" in
-        linux/386)
-            ARCH="32"
-            ;;
-        linux/amd64)
-            ARCH="64"
-            ;;
-        linux/arm/v6)
-            ARCH="arm32-v6"
-            ;;
-        linux/arm/v7)
-            ARCH="arm32-v7a"
-            ;;
-        linux/arm64|linux/arm64/v8)
-            ARCH="arm64-v8a"
-            ;;
-        linux/ppc64le)
-            ARCH="ppc64le"
-            ;;
-        linux/s390x)
-            ARCH="s390x"
-            ;;
-        *)
-            ARCH=""
-            ;;
-    esac
+if [[ "$(uname)" == 'Linux' ]]; then
+  case "$(uname -m)" in
+    'i386' | 'i686')
+      ARCH='32'
+      ;;
+    'amd64' | 'x86_64')
+      ARCH='64'
+      ;;
+    'armv5tel')
+      ARCH='arm32-v5'
+      ;;
+    'armv6l')
+      ARCH='arm32-v6'
+      ;;
+    'armv7' | 'armv7l')
+      ARCH='arm32-v7a'
+      ;;
+    'armv8' | 'aarch64')
+      ARCH='arm64-v8a'
+      ;;
+    'mips')
+      ARCH='mips32'
+      ;;
+    'mipsle')
+      ARCH='mips32le'
+      ;;
+    'mips64')
+      ARCH='mips64'
+      ;;
+    'mips64le')
+      ARCH='mips64le'
+      ;;
+    'ppc64')
+      ARCH='ppc64'
+      ;;
+    'ppc64le')
+      ARCH='ppc64le'
+      ;;
+    'riscv64')
+      ARCH='riscv64'
+      ;;
+    's390x')
+      ARCH='s390x'
+      ;;
+    *)
+      ARCH=""
+      ;;
+  esac
 fi
 [ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 
 # Download files
 V2RAY_FILE="v2ray-linux-${ARCH}.zip"
 DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
+TMP_DIRECTORY="$(mktemp -d)"
+V2RAY_PATH="${TMP_DIRECTORY}/${V2RAY_FILE}"
+DGST_PATH="${TMP_DIRECTORY}/${DGST_FILE}"
 echo "Downloading binary file: ${V2RAY_FILE}"
 echo "Downloading binary file: ${DGST_FILE}"
 
-TAG=$(wget -qO- https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
-wget -O ${PWD}/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE} > /dev/null 2>&1
-wget -O ${PWD}/v2ray.zip.dgst https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE} > /dev/null 2>&1
+TAG=$(curl -fsSL https://raw.githubusercontent.com/v2fly/docker/master/ReleaseTag | head -n1)
+curl -#Lo ${V2RAY_PATH} https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${V2RAY_FILE}
+curl -fsSLo ${DGST_PATH} https://github.com/v2fly/v2ray-core/releases/download/${TAG}/${DGST_FILE}
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to download binary file: ${V2RAY_FILE} ${DGST_FILE}" && exit 1
@@ -50,22 +70,22 @@ fi
 echo "Download binary file: ${V2RAY_FILE} ${DGST_FILE} completed"
 
 # Check SHA512
-LOCAL=$(openssl dgst -sha512 v2ray.zip | sed 's/([^)]*)//g')
-STR=$(cat v2ray.zip.dgst | grep 'SHA512' | head -n1)
+LOCAL=$(openssl dgst -sha512 ${V2RAY_PATH} | sed 's/([^)]*)//g')
+STR=$(cat ${DGST_PATH} | grep 'SHA512' | head -n1)
 
 if [ "${LOCAL}" = "${STR}" ]; then
-    echo " Check passed" && rm -fv v2ray.zip.dgst
+    echo " Check passed" && rm -fv ${DGST_PATH}
 else
     echo " Check have not passed yet " && exit 1
 fi
 
 # Prepare
 echo "Prepare to use"
-unzip v2ray.zip && chmod +x v2ray v2ctl
+unzip ${V2RAY_PATH} -d ${TMP_DIRECTORY} && cd ${TMP_DIRECTORY} && chmod +x v2ray v2ctl
 mv v2ray v2ctl /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
 mv config.json /etc/v2ray/config.json
 
 # Clean
-rm -rf ${PWD}/*
+rm -rf ${TMP_DIRECTORY}
 echo "Done"
